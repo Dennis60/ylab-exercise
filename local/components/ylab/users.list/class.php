cf922e2974b0264ad56b9ad0656a8b24f6c6f66e
dennis60 <dennis60@mail.ru>
2018-08-07 22:45:30 +0300

    Переписано при помощи методов ядра D7

--- local/components/ylab/users.list/class.php
+++ local/components/ylab/users.list/class.php
@@ -29,21 +29,31 @@ class UsersListComponent extends \CBitrixComponent
      * Возвращает массив активных записей из инфоблока "Пользователи"
      *
      * @return array
+     * @throws \Bitrix\Main\LoaderException
      */
     protected function getUsersList()
     {
         $arUsers = array();
 
-        if (CModule::IncludeModule("iblock")) {
-            $arOrder = Array("ID" => "ASC");
-            $arFilter = Array("IBLOCK_ID" => self::USERS_IBLOCK_ID, "ACTIVE" => "Y");
+        if (\Bitrix\Main\Loader::includeModule("iblock")) {
             $arSelect = Array("ID", "NAME");
-            $oUsers = CIBlockElement::GetList($arOrder, $arFilter, false, false, $arSelect);
+            $arFilter = Array("IBLOCK_ID" => self::USERS_IBLOCK_ID, "ACTIVE" => "Y");
 
-            while ($arUser = $oUsers->GetNext(false, false)) {
+            try {
+                $oUsers = \Bitrix\Iblock\ElementTable::getList(array('select' => $arSelect, 'filter' => $arFilter));
+            } catch (\Bitrix\Main\ObjectPropertyException $e) {
+                $e->getMessage();
+            } catch (\Bitrix\Main\ArgumentException $e) {
+                $e->getMessage();
+            } catch (\Bitrix\Main\SystemException $e) {
+                $e->getMessage();
+            }
+
+            foreach ($oUsers->fetchAll() as $arUser) {
                 $arUsers[] = $arUser;
             }
         }
+
         return $arUsers;
     }
 }
